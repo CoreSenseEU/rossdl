bdeeb0a147720bf37ccb124221824f868d8395cd
Francisco Martín Rico <fmrico@gmail.com>
2023-04-03 20:48:11 +0200

    Add tests and CI
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- rossdl_test/include/rossdl_test/Nodes2.hpp
+++ rossdl_test/include/rossdl_test/Nodes2.hpp
@@ -1,5 +1,16 @@
-// generated from rossdl_cmake/resource/nodes.hpp.em
-// generated code does not contain a copyright notice
+// Copyright 2023 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef ROSSDL_TEST__NODES2_HPP_
 #define ROSSDL_TEST__NODES2_HPP_
@@ -105,7 +116,6 @@ protected:
   typename rclcpp::PublisherBase::SharedPtr
   get_publisher_base(const std::string & id)
   {
-
     RCLCPP_ERROR(get_logger(), "Not publishers found");
     return nullptr;
   }
